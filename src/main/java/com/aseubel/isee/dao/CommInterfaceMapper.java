18d911be8e273f5f910d26a19c45bd2433f7c477
Aseubel <1228392319@qq.com>
2025-04-18 13:28:14 +0800

    feat: 害虫检测

---
+++ src/main/java/com/aseubel/isee/dao/CommInterfaceMapper.java
@@ -0,0 +1,15 @@
+package com.aseubel.isee.dao;
+
+import com.aseubel.isee.pojo.entity.CommInterface;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Aseubel
+ * @date 2025/4/18 上午12:10
+ */
+@Mapper
+public interface CommInterfaceMapper extends BaseMapper<CommInterface> {
+
+
+}
